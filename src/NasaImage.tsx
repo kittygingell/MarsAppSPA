baf19de22d518a29abc00f58060797c046b4558e
Kitty <kittygingell@gmail.com>
2021-07-07 14:14:06 +0100

    Logo

---
+++ src/NasaImage.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+export default class NasaImage extends Component {
+
+    render() {
+        return (<img src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png" height="300" width="600" />
+        )
+    }
+
+}
--- src/ParagraphsWithImage.tsx
+++ src/ParagraphsWithImage.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NasaParagraph from "./NasaParagraph";
+import NasaImage from "./NasaImage";
 export default class ParagraphsWithImage extends Component {
 
     render() {
@@ -8,8 +9,8 @@ export default class ParagraphsWithImage extends Component {
         <div>
             <NasaParagraph/>
         </div>
-        <img src="https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fmars.nasa.gov%2Fmars2020%2F&psig=AOvVaw2v-3AJz2wad5zXq0eynUwR&ust=1625741968783000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCPCcvPbm0PECFQAAAAAdAAAAABAD"/>
-        <div>
+                <NasaImage/>
+                <div>
             <p>A Mars Rover</p>
         </div></>
         )
