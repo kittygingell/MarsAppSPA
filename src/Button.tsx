cdf2c49f00773782d3faa6a439dc519331e623a4
Ricky Kawagishi <ricky.kawagishi@softwire.com>
2021-07-07 15:17:33 +0100

    Used local storage to make the count persist

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,10 +3,22 @@ import React, { useState } from 'react';
 export default function Button() {
     // Declare a new state variable, which we'll call "count"
     const [count, setCount] = useState(0);
+    const storage = window.localStorage
+    const countVariableName = 'count'
+    const current = storage.getItem(countVariableName)
+    if (current == null) {
+        storage.setItem(countVariableName, '0')
+    }
+
     return (
         <div>
-            <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
+            <p>You clicked {count === 0 ? Number(storage.getItem(countVariableName)) || 0 : count} times</p>
+            <button onClick={() => {
+                const currentCount = Number(storage.getItem(countVariableName))
+                const newCount = currentCount + 1
+                storage.setItem(countVariableName, String(newCount))
+                setCount(newCount)
+            }}>
                 Click me
             </button>
         </div>
